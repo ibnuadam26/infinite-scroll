a52bafa9f661d7222b06aa331d4d8249e12b9da5
Ibnu Adam <ibnuadam600@gmail.com>
2023-03-10 19:59:22 +0700

    remove useless code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 
-function App() {
+const style = {
+  height: 30,
+  border: "1px solid green",
+  margin: 6,
+  padding: 8,
+};
+
+export default function App() {
+  const [items, setItems] = useState(Array.from({ length: 20 }, (_, index) => ({
+    id: index,
+    content: `Item ${index}`
+  })));
+  const [hasMore, setHasMore] = useState(true);
+
+  useEffect(() => {
+    if (items.length >= 100) {
+      setHasMore(false);
+    }
+  }, [items]);
+
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      const newItems = Array.from({ length: 20 }, (_, index) => ({
+        id: items.length + index,
+        content: `Item ${items.length + index}`
+      }));
+
+      setItems([...items, ...newItems]);
+    }, 1500);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>demo: react-infinite-scroll-component</h1>
+      <hr />
+      <InfiniteScroll
+        dataLength={items.length}
+        next={fetchMoreData}
+        hasMore={hasMore}
+        loader={<h4>Loading...</h4>}
+      >
+        {items.map((i) => (
+          <div style={style} key={i.id}>
+            {i.content}
+          </div>
+        ))}
+      </InfiniteScroll>
     </div>
   );
 }
-
-export default App;
